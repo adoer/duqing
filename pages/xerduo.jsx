296512042c64ae399aec68145bcb6c0ccbdf8828
duer <957965700@qq.com>
2022-12-08 20:22:21 +0800

    Added      public/xerduo1.jpg Modified   pages/xerduo.jsx

--- pages/xerduo.jsx
+++ pages/xerduo.jsx
@@ -1,8 +1,9 @@
 const xerduo = function () {
   return (
     <>
-      <a href="https://www.xerduo.com" target="_blank">
-        <img style={{ width: "100%" }} src="./xerduo.jpg" alt="点击访问" title="点击访问" />
+      <a style={{ textAlign: "center", display: "block" }} href="https://www.xerduo.com" target="_blank" title="点击访问">
+        <img style={{ width: "100%", marginBottom: "1rem" }} src="./xerduo1.jpg" alt="点击访问" />
+        <span>xerduo.com</span>
       </a>
     </>
   )
