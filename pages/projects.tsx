092be2e8be15c97f6e1443b7d3a15df18c968fd1
Du Qing <duqingBetter@qq.com>
2023-03-08 22:04:09 +0800

    Added      pages/projects.tsx

---
+++ pages/projects.tsx
@@ -0,0 +1,41 @@
+import ProjectList from '../components/projectList/ProjectList'
+const Projects = function () {
+  const data: any[] = [
+    {
+      title: 'xerduo',
+      herf: 'https://xerduo.com',
+      des: 'A blog base on Hexo',
+    },
+    {
+      title: 'ChatGPT',
+      herf: 'https://chatgpt.duqing.ink',
+      des: 'AI-powered chatbot',
+    },
+    {
+      title: 'Tetris',
+      herf: 'https://tetris.duqing.ink',
+      des: 'Tetris game with AI 🎮',
+    },
+    {
+      title: 'BPM design editor',
+      herf: 'https://bpm.duqing.ink',
+      des: 'Design editor for BPM',
+    },
+    {
+      title: 'Auto-i18n',
+      herf: 'https://github.com/adoer/auto-i18n',
+      des: 'A tool for vue-i18n use',
+    },
+    {
+      title: 'ImgUpload',
+      herf: 'https://imgupload.duqing.ink',
+      des: 'Cropping image',
+    },
+  ]
+  return (
+    <>
+      <ProjectList data={data}></ProjectList>
+    </>
+  )
+}
+export default Projects
